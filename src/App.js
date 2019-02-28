7ba78ae8e2a7841f2db421c9b45f85579ca4f4dc
John Tenezaca <john.tenezaca7@gmail.com>
2019-02-28 15:25:59 -0500

    created layout component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import Counter from "./containers/Counter";
+import Layout from "./components/Layout";
 
 class App extends Component {
   render() {
     return (
-      <Counter />
+      <Layout>
+      </Layout>
     );
   }
 }
---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+ class Layout extends Component {
+  render() {
+    const { name } = this.props;
+
+    return (
+      <div className="counter">
+        <h1>Main Layout. Prop test: { name }.</h1>
+      </div>
+    );
+  }
+}
+
+Layout.propTypes = {
+  name: PropTypes.string
+};
+
+export default Layout;
